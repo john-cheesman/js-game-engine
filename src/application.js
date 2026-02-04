a8e1d14dc6828d557814e045f9d7a177606f66ff
John Cheesman <john-cheesman@users.noreply.github.com>
2026-02-04 16:46:33 +0000

    Add gamepad touch events

--- src/application.js
+++ src/application.js
@@ -1,46 +1,62 @@
 export default class Application {
-  constructor (w, h) {
-    this.width = w
-    this.height = h
-    this.canvas = document.createElement('canvas')
-    this.canvas.width = w
-    this.canvas.height = h
-    this.context = this.canvas.getContext('2d')
-    this.context.imageSmoothingEnabled = false
-    const appContainer = document.getElementById('app')
-    appContainer.append(this.canvas)
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = w;
+    this.canvas.height = h;
+    this.context = this.canvas.getContext("2d");
+    this.context.imageSmoothingEnabled = false;
+    const appContainer = document.getElementById("app");
+    appContainer.append(this.canvas);
 
     const keyControls = [
-      'KeyZ',
-      'KeyX',
-      'ArrowUp',
-      'ArrowDown',
-      'ArrowLeft',
-      'ArrowRight'
-    ]
+      "KeyZ",
+      "KeyX",
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+    ];
 
-    this.keys = {}
-    this.pads = []
+    this.keys = {};
+    this.pads = [];
 
-    document.addEventListener('keydown', (e) => {
+    document.addEventListener("keydown", (e) => {
       if (keyControls.includes(e.code)) {
-        this.keys[e.code] = true
+        this.keys[e.code] = true;
       }
-    })
+    });
 
-    document.addEventListener('keyup', (e) => {
+    document.addEventListener("keyup", (e) => {
       if (this.keys[e.code]) {
-        this.keys[e.code] = false
+        this.keys[e.code] = false;
       }
-    })
+    });
 
-    document.addEventListener('gamepadconnected', (e) =>
+    document.addEventListener("gamepadconnected", (e) =>
       this.pads.push(e.gamepad.index)
-    )
+    );
 
-    document.addEventListener('gamepaddisconnected', (e) => {
-      const index = this.pads.findIndex(e.gamepad.index)
-      delete this.pads[index]
-    })
+    document.addEventListener("gamepaddisconnected", (e) => {
+      const index = this.pads.findIndex(e.gamepad.index);
+      delete this.pads[index];
+    });
+
+    const gamepadBtns = document.querySelectorAll("[data-btn]");
+
+    gamepadBtns.forEach((btn) => {
+      btn.addEventListener("touchstart", (e) => {
+        if (keyControls.includes(e.currentTarget.dataset.btn)) {
+          this.keys[e.currentTarget.dataset.btn] = true;
+        }
+      });
+
+      btn.addEventListener("touchend", (e) => {
+        if (this.keys[e.currentTarget.dataset.btn]) {
+          this.keys[e.currentTarget.dataset.btn] = false;
+        }
+      });
+    });
   }
 }
